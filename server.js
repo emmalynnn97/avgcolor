f18fc6f22e4fb612e21fd7af6bdfe10b09cb4e43
woodpav <devwoodpav@gmail.com>
2021-03-31 20:50:32 -0700

    Save sorted images to public/sorted folder

--- server.js
+++ server.js
@@ -1,21 +1,43 @@
 import { getAverageColor } from 'fast-average-color-node';
 import colorSort from 'color-sorter'
-import fs from 'fs'
-const list=[]
-const colors=[]
-  
-const testFolder = './public/images/';
+import { promises as fs } from 'fs'
+const list = []
 
-fs.readdir(testFolder, (err, files) => {
-    files.forEach(file => {
-        getAverageColor(`./public/images/${file}`).then(color => {
-            colors.push(color.rgb)
-            list.push({file:file,color:color.rgb})
-            var sorted = colors.sort(colorSort.sortFn)
-          })
-    });
-  });
-  
+const inFolder = './public/images';
+const outFolder = './public/sorted'
 
+const files = await fs.readdir(inFolder);
+
+const colors = await Promise.all(
+  files.map(async file => {
+    console.log(file)
+    try {
+      const color = await getAverageColor(`${inFolder}/${file}`);
+      return { file, color: color.rgb };
+    } catch (e) {
+      console.log(e)
+    }
+
+  })
+);
+
+var sorted = colors.filter(c => c != null).sort(
+  ({ color: color1 }, { color: color2 }) => colorSort.sortFn(color1, color2)
+);
+
+
+
+await fs.rmdir(outFolder, { recursive: true });
+await fs.mkdir(outFolder);
+
+await Promise.all(
+  sorted.map(async ({ color, file }, index) => {
+    try {
+      return await fs.copyFile(`${inFolder}/${file}`, `${outFolder}/${index}.png`)
+    } catch (e) {
+      console.log(e)
+    }
+  })
+)
 
 
